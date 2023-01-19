23c9ef6ab7c1dd4ba10ee089a655c871b5391780
zeroet <y36609672@gmail.com>
2023-01-20 00:23:49 +0100

    make query

---
+++ server.js
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from "apollo-server"
+
+const typeDefs = gql`
+    type User{
+        id:ID
+        username: String
+    }
+    type Tweet{
+        id: ID
+        text: String
+        author: User
+    }
+    type Query{
+        allTweets: [Tweet!]!
+        tweet(id: ID!): Tweet
+    }
+    type Mutation {
+        postTweet(text: String!, userID: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
+    }
+`;
+
+const server = new ApolloServer({typeDefs})
+
+server.listen().then(({url}) => {
+    console.log(`Running on ${url}`);
+});
